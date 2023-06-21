adf72d697867e81b516c8eb4c380beb29b445b72
wangxin6 <wangxin6@tiens.com>
2023-06-21 15:55:02 +0800

    init

--- lib/animation/animated_listView.dart
+++ lib/animation/animated_listView.dart
@@ -4,67 +4,53 @@ import 'package:flutter/material.dart';
 class AnimatedListViewWidget extends BasefulWidget {
   AnimatedListViewWidget(super.title, {super.key});
 
-
   @override
   State<AnimatedListViewWidget> createState() => AnimatedListViewWidgetState();
 }
 
 class AnimatedListViewWidgetState extends State<AnimatedListViewWidget> {
   final GlobalKey<AnimatedListState> _listKey = GlobalKey<AnimatedListState>();
-  late ListModel<int> _list;
-  int? _selectedItem;
-  late int _nextItem;
+  late List<int> _list;
 
   @override
   void initState() {
     super.initState();
-    _list = ListModel<int>(
-      listKey: _listKey,
-      initialItems: <int>[0, 1, 2],
-      removedItemBuilder: _buildRemovedItem,
-    );
-    _nextItem = 3;
+    _list = [1, 2, 3];
   }
 
   Widget _buildItem(
       BuildContext context, int index, Animation<double> animation) {
-    return CardItem(
-      animation: animation,
-      item: _list[index],
-      selected: _selectedItem == _list[index],
-      onTap: () {
-        setState(() {
-          _selectedItem = _selectedItem == _list[index] ? null : _list[index];
-        });
-      },
-    );
-  }
-  Widget _buildRemovedItem(
-      int item, BuildContext context, Animation<double> animation) {
-    return CardItem(
-      animation: animation,
-      item: item,
-      // No gesture detector here: we don't want removed items to be interactive.
-    );
+    return SlideTransition(
+        position:
+            Tween<Offset>(begin: const Offset(0, 0), end: const Offset(0, 1))
+                .animate(animation),
+        child: Text(
+          "${_list[index]}",
+          style: const TextStyle(fontSize: 50),
+        ));
   }
 
   void _insert() {
-    final int index =
-    _selectedItem == null ? _list.length : _list.indexOf(_selectedItem!);
-    _list.insert(index, _nextItem++);
+    var i = (_list.last+1);
+    _list.add(i);
+    _listKey.currentState?.insertItem(_list.length-1);
   }
 
   void _remove() {
-    if (_selectedItem != null) {
-      _list.removeAt(_list.indexOf(_selectedItem!));
-      setState(() {
-        _selectedItem = null;
-      });
-    }
+    var removeLast = _list.removeLast();
+    _listKey.currentState?.removeItem(
+        _list.length+1,
+        (context, animation) => FadeTransition(
+            opacity: animation,
+            child:  Text(
+              "$removeLast",
+              style: const TextStyle(fontSize: 50),
+            )));
   }
 
   @override
   Widget build(BuildContext context) {
+    print("build");
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -85,91 +71,10 @@ class AnimatedListViewWidgetState extends State<AnimatedListViewWidget> {
         body: Padding(
           padding: const EdgeInsets.all(16.0),
           child: AnimatedList(
+            padding: EdgeInsets.zero,
             key: _listKey,
             initialItemCount: _list.length,
             itemBuilder: _buildItem,
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-typedef RemovedItemBuilder<T> = Widget Function(
-    T item, BuildContext context, Animation<double> animation);
-class ListModel<E> {
-  ListModel({
-    required this.listKey,
-    required this.removedItemBuilder,
-    Iterable<E>? initialItems,
-  }) : _items = List<E>.from(initialItems ?? <E>[]);
-
-  final GlobalKey<AnimatedListState> listKey;
-  final RemovedItemBuilder<E> removedItemBuilder;
-  final List<E> _items;
-
-  AnimatedListState? get _animatedList => listKey.currentState;
-
-  void insert(int index, E item) {
-    _items.insert(index, item);
-    _animatedList!.insertItem(index);
-  }
-
-  E removeAt(int index) {
-    final E removedItem = _items.removeAt(index);
-    if (removedItem != null) {
-      _animatedList!.removeItem(
-        index,
-            (BuildContext context, Animation<double> animation) {
-          return removedItemBuilder(removedItem, context, animation);
-        },
-      );
-    }
-    return removedItem;
-  }
-
-  int get length => _items.length;
-
-  E operator [](int index) => _items[index];
-
-  int indexOf(E item) => _items.indexOf(item);
-}
-
-class CardItem extends StatelessWidget {
-  const CardItem({
-    super.key,
-    this.onTap,
-    this.selected = false,
-    required this.animation,
-    required this.item,
-  })  : assert(item >= 0);
-
-  final Animation<double> animation;
-  final VoidCallback? onTap;
-  final int item;
-  final bool selected;
-
-  @override
-  Widget build(BuildContext context) {
-    TextStyle textStyle = Theme.of(context).textTheme.headlineMedium!;
-    if (selected) {
-      textStyle = textStyle.copyWith(color: Colors.lightGreenAccent[400]);
-    }
-    return Padding(
-      padding: const EdgeInsets.all(2.0),
-      child: SizeTransition(
-        sizeFactor: animation,
-        child: GestureDetector(
-          behavior: HitTestBehavior.opaque,
-          onTap: onTap,
-          child: SizedBox(
-            height: 80.0,
-            child: Card(
-              color: Colors.primaries[item % Colors.primaries.length],
-              child: Center(
-                child: Text('Item $item', style: textStyle),
-              ),
-            ),
           ),
         ),
       ),
